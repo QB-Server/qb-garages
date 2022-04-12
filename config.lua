1c37ac7d9798a4a864b73a8cf1adb3cc942f6216
Bobbins <bobbins@protonmail.com>
2022-04-12 15:27:13 +0100

    Change garage names

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ SharedGarages = false   --True == Gang and job garages are shared, false == Gang
 
 Garages = {
     ["motelgarage"] = {
-        label = "Motel Parking",
+        label = "Garage: A",
         takeVehicle = vector3(273.43, -343.99, 44.91),
         spawnPoint = vector4(270.94, -342.96, 43.97, 161.5),
         putVehicle = vector3(276.69, -339.85, 44.91),
@@ -14,7 +14,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["sapcounsel"] = {
-        label = "San Andreas Parking",
+        label = "Garage: B",
         takeVehicle = vector3(-330.01, -780.33, 33.96),
         spawnPoint = vector4(-334.44, -780.75, 33.96, 137.5),
         putVehicle = vector3(-336.31, -774.93, 33.96),
@@ -25,7 +25,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["spanishave"] = {
-        label = "Spanish Ave Parking",
+        label = "Garage: C",
         takeVehicle = vector3(-1160.86, -741.41, 19.63),
         spawnPoint = vector4(-1163.88, -749.32, 18.42, 35.5),
         putVehicle = vector3(-1147.58, -738.11, 19.31),
@@ -36,7 +36,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["caears24"] = {
-        label = "Caears 24 Parking",
+        label = "Garage: D",
         takeVehicle = vector3(69.84, 12.6, 68.96),
         spawnPoint = vector4(73.21, 10.72, 68.83, 163.5),
         putVehicle = vector3(65.43, 21.19, 69.47),
@@ -47,7 +47,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["caears242"] = {
-        label = "Caears 24 Parking",
+        label = "Garage: E",
         takeVehicle = vector3(-475.31, -818.73, 30.46),
         spawnPoint = vector4(-472.03, -815.47, 30.5, 177.5),
         putVehicle = vector3(-453.6, -817.08, 30.61),
@@ -58,7 +58,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["lagunapi"] = {
-        label = "Laguna Parking",
+        label = "Garage: F",
         takeVehicle = vector3(364.37, 297.83, 103.49),
         spawnPoint = vector4(367.49, 297.71, 103.43, 340.5),
         putVehicle = vector3(363.04, 283.51, 103.38),
@@ -69,7 +69,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["airportp"] = {
-        label = "Airport Parking",
+        label = "Garage: G",
         takeVehicle = vector3(-796.86, -2024.85, 8.88),
         spawnPoint = vector4(-800.41, -2016.53, 9.32, 48.5),
         putVehicle = vector3(-804.84, -2023.21, 9.16),
@@ -80,7 +80,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["beachp"] = {
-        label = "Beach Parking",
+        label = "Garage: H",
         takeVehicle = vector3(-1183.1, -1511.11, 4.36),
         spawnPoint = vector4(-1181.0, -1505.98, 4.37, 214.5),
         putVehicle = vector3(-1176.81, -1498.63, 4.37),
@@ -91,7 +91,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["themotorhotel"] = {
-        label = "The Motor Hotel Parking",
+        label = "Garage: I",
         takeVehicle = vector3(1137.77, 2663.54, 37.9),            
         spawnPoint = vector4(1137.69, 2673.61, 37.9, 359.5),      
         putVehicle = vector3(1137.75, 2652.95, 37.9),
@@ -102,7 +102,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["liqourparking"] = {
-        label = "Liqour Parking",
+        label = "Garage: J",
         takeVehicle = vector3(934.95, 3606.59, 32.81),
         spawnPoint = vector4(941.57, 3619.99, 32.5, 141.5),
         putVehicle = vector3(939.37, 3612.25, 32.69),
@@ -113,7 +113,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["shoreparking"] = {
-        label = "Shore Parking",
+        label = "Garage: K",
         takeVehicle = vector3(1726.21, 3707.16, 34.17),
         spawnPoint = vector4(1730.31, 3711.07, 34.2, 20.5),
         putVehicle = vector3(1737.13, 3718.91, 34.04),
@@ -124,7 +124,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["haanparking"] = {
-        label = "Bell Farms Parking",
+        label = "Garage: L",
         takeVehicle = vector3(78.34, 6418.74, 31.28),
         spawnPoint = vector4(70.71, 6425.16, 30.92, 68.5), 
         putVehicle = vector3(85.3, 6427.52, 31.33),
@@ -135,7 +135,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["dumbogarage"] = {
-        label = "Dumbo Private Parking",
+        label = "Garage: M",
         takeVehicle = vector3(157.26, -3240.00, 7.00),
         spawnPoint = vector4(165.32, -3236.10, 5.93, 268.5), 
         putVehicle = vector3(165.32, -3230.00, 5.93),
@@ -146,7 +146,7 @@ Garages = {
         vehicle = 'car'                 --car, air, sea
     },
     ["pillboxgarage"] = {
-        label = "Pillbox Garage Parking",
+        label = "Garage: N",
         takeVehicle = vector3(215.9499, -809.698, 30.731),
         spawnPoint = vector4(234.1942, -787.066, 30.193, 159.6),
         putVehicle = vector3(218.0894, -781.370, 30.389),
@@ -167,7 +167,7 @@ Garages = {
     --    vehicle = 'car'                 --car, air, sea
     --},
     ["impoundlot"] = {
-        label = "Impound Lot",
+        label = "Impound",
         takeVehicle = vector3(409.89, -1623.51, 29.29),
         spawnPoint = vector4(407.92, -1646.29, 29.29, 226.39),
         showBlip = true,
